e41cc1063980c7c9bb52443760035bd5488f8695
aliennn04 <efendir662@gmail.com>
2023-09-24 19:09:15 -0700

    push projek

---
+++ kasir.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int jum_belanja,bayar,diskon,jumlah[50],harga[50],
+    total[50];
+    string nama_barang[50];
+    float;
+    cout<<"-----PROGRAM KASIR--------"<<endl;
+    cout<<endl;
+    cout<<"masukan jumlah belanja :";
+    cin>>jum_belanja;
+    
+}
